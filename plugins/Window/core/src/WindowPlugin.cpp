6fb1c208673bc95250ec9e28a56440e3851f7e22
KrytaniK <68296788+KrytaniK@users.noreply.github.com>
2025-03-01 13:11:29 -0600

    Removed GLFW test code

--- plugins/Window/core/src/WindowPlugin.cpp
+++ plugins/Window/core/src/WindowPlugin.cpp
@@ -1,7 +1,6 @@
 #include "../macros/AurionExport.h"
 
 #include <iostream>
-#include <GLFW/glfw3.h>
 
 import Aurion.WindowPlugin;
 import Aurion.Window;
@@ -10,32 +9,12 @@ namespace Aurion
 {
 	WindowPlugin::WindowPlugin()
 	{
-		if (!glfwInit())
-		{
-			std::cout << "[Window Plugin] GLFW Initialization Failed!" << std::endl;
-		}
-		else
-			std::cout << "[Window Plugin] GLFW Initialization Succeeded!" << std::endl;
-
-		// Create a test window
-		GLFWwindow* window = glfwCreateWindow(640, 480, "My Title", NULL, NULL);
-		if (!window)
-		{
-			std::cout << "[Window Plugin] GLFW Window Creation or OpenGL Context Creation Failed!" << std::endl;
-		}
-		else
-		{
-			std::cout << "[Window Plugin] Window Created Successfully!!!!" << std::endl;
-			// Destroy Test window
-			glfwDestroyWindow(window);
-		}
-
 		
 	}
 
 	WindowPlugin::~WindowPlugin()
 	{
-		glfwTerminate();
+		
 	}
 
 	void WindowPlugin::Initialize(IPluginContext* context)
